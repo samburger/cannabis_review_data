b842bf7af0814f7031f150bcd57e4280f711d141
Sam Friedman <githubsamburger@n23.mm.st>
2021-12-29 02:57:53 -0500

    first pass at scraping reviews

--- scrape_leafly_reviews.py
+++ scrape_leafly_reviews.py
@@ -1,84 +1,91 @@
-import pandas as pd
-from playwright.sync_api import sync_playwright
-
+import json
+from json.decoder import JSONDecodeError
+import math
+import os
 import random
+import time
+from itertools import chain
 
-with open("useragents.txt", "r") as f:
-    USER_AGENTS = f.readlines()
+import pandas as pd
+import requests
+from requests.exceptions import RequestException
+from tqdm import tqdm, trange
 
-HEADERS = {
-    "Accept": (
-        "text/html,application/xhtml+xml,"
-        "application/xml;q=0.9,image/avif,image/webp,"
-        "*/*;q=0.8"
-    ),
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Cache-Control": "max-age=0",
-    "Connection": "keep-alive",
-    "DNT": "1",
-    "Host": "www.leafly.com",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
-    "TE": "trailers",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": random.choice(USER_AGENTS),
-}
+STRAINS_API_URL = (
+    "https://consumer-api.leafly.com/api/strain_playlists/v2?&skip={}&take={}"
+)
+REVIEWS_API_URL = (
+    "https://www.leafly.com/web-strain-explorer/api/strains/{}/reviews?take=50&page={}"
+)
+NUM_STRAINS = 5867
 
-with sync_playwright() as playwright:
-    # Setup
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
 
-    # Get URLs of all strain pages
-    # Load strains list
-    page.goto(
-        "https://www.leafly.com/strains",
-        wait_until="networkidle",
-    )
-    # Close age gate
-    page.set_checked("id=remember-user-checkbox", checked=True, force=True)
-    page.click("id=tou-continue")
-    # Check number of strains
-    num_strains = int(
-        page.query_selector("text=/\d+ strains/i").inner_text().split()[0]
-    )
-    # Check result pages count
-    num_pages = int(page.query_selector("text=/\d+ of \d+/i").inner_text().split()[-1])
-    data = list()
-    count = 0
-    # TODO: see if we actually get the data for all cards/pages in a single XHR
-    while page.is_visible("text=Next", strict=True) and (count < 5):
-        count += 1
-        page.locator("[itemprop='name']").last.wait_for()
-        # Grab strain name + url for each strain on page
-        strain_cards = page.locator("data-testid=strain-list__strain-card")
-        strain_values = list(
-            map(lambda x: x.split("\n")[1:], strain_cards.all_inner_texts())
-        )
-        df = pd.DataFrame(
-            {
-                "Type": [strain[0] for strain in strain_values if strain[0]],
-                "Name": [strain[1] for strain in strain_values],
-                "Slug": [
-                    strain.get_attribute("href")
-                    for strain in strain_cards.locator("[class=p-md]").element_handles()
-                ],
-            }
-        )
-        df = df[df["Type"].isin(["Hybrid", "Indica", "Sativa"])]
-        data.append(df)
-        # Load next page
-        page.query_selector("text=Next").click()
-    # Merge each page's results
-    data = pd.concat(data, ignore_index=True)
-    data["URL"] = data["Slug"].apply(lambda x: f"https://leafly.com{x}")
-    print("Breakme")
+def get_strain_metadata():
+    if os.path.exists("strains_metadata.json"):
+        # Load existing data dump
+        with open("strains_metadata.json", "r") as f:
+            strain_list = json.load(f)
+    else:
+        strains_raw = []
+        for skip in trange(0, NUM_STRAINS, 50):
+            r = requests.get(STRAINS_API_URL.format(skip, 50))
+            data = json.loads(r.content)
+            strains_raw.append(data)
+        batches = [batch["hits"]["strain"] for batch in strains_raw]
+        strain_list = list(chain.from_iterable(batches))
+        with open("strains_metadata.json", "w") as f:
+            json.dump(strain_list, f)
+    return strain_list
 
-    # Load /reviews page for each strain url
-    # Check number of reviews and pages
-    # for page in range(page_max_reviews)
-    # copy pseudoname, review, date, ratings, etc.
+
+if __name__ == "__main__":
+    # Make a dataframe of strain metadata
+    strain_list = get_strain_metadata()
+    strains = pd.DataFrame()
+    for col in ["id", "category", "name", "reviewCount", "slug"]:
+        strains[col] = [s[col] for s in strain_list]
+    strains["url"] = "https://leafly.com/strain" + strains["slug"]
+    for chem in ["cbc", "cbd", "cbg", "thc", "thcv"]:
+        strains[f"{chem}_p50"] = [
+            s["cannabinoids"][chem]["percentile50"] for s in strain_list
+        ]
+    strains = strains.rename(columns={"id": "strain_id", "reviewCount": "review_count"})
+    strains = strains.set_index("strain_id")
+
+    # Make a dataframe of review data
+    reviews = []
+    for sid in tqdm(strains.sort_values(by=["review_count"], ascending=False).index):
+        reviews_raw = []
+        n_reviews = strains.at[sid, "review_count"]
+        n_pages = math.ceil(n_reviews / 50)
+        for page in trange(1, n_pages + 1):
+            reviews_uri = REVIEWS_API_URL.format(strains.at[sid, "slug"], page)
+            r = requests.get(reviews_uri)
+            if not r.ok:
+                raise RequestException
+            reviews_raw.append(json.loads(r.content)["data"])
+            time.sleep(random.gauss(3, 1))
+        # Why not always as many elements as n_reviews?
+        review_list = list(chain.from_iterable(reviews_raw))
+        df = pd.DataFrame()
+        for col in [
+            "id",
+            "username",
+            "created",
+            "form",
+            "language",
+            "rating",
+            "upvotesCount",
+            "text",
+        ]:
+            df[col] = [r[col] for r in review_list]
+        df["strain_id"] = [
+            strains.loc[strains["slug"] == r["strainSlug"], :].index[0]
+            for r in review_list
+        ]
+        df = df.rename(columns={"id": "review_id", "upvotesCount": "upvotes"})
+        reviews.append(df)
+        # Checkpoint
+        if len(reviews) % 10 == 0:
+            with open("reviews_data.json", "w") as f:
+                json.dump(reviews, f)
